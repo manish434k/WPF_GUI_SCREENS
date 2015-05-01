1dce339270ccf3fb0d6c0569fc63d73600c0ea18
Manish <manish434k@yahoo.com>
2015-05-02 05:00:17 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Steps to create ribbon in WPF
+>> right click on "references" in solution and add "System.Windows.Controls.Ribbon"
+
+>>Use </RibbonWindow> instead of </Window>.
+
+>>Create Ribbon tipe user control and include in main window.
+
---
+++ npeimanovic_wpf/RibbonControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Ribbon;
+
+namespace npeimanovic_wpf
+{
+    /// <summary>
+    /// Interaction logic for RibbonControl.xaml
+    /// </summary>
+    public partial class RibbonControl : Ribbon
+    {
+        public RibbonControl()
+        {
+            InitializeComponent();
+        }
+        private void add_clicked(object sender, RoutedEventArgs e)
+        {
+            UserControlAddEdit subWindow = new UserControlAddEdit();
+            subWindow.Show();
+        }
+    }
+}
