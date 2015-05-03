abe3e1d14c60ee2b2382a27adbff4ee3333664bc
Manish <manish434k@yahoo.com>
2015-05-03 14:00:34 +0530

    Milestone 1 delivery

--- npeimanovic_wpf/MainWindow.xaml.cs
+++ npeimanovic_wpf/MainWindow.xaml.cs
@@ -19,8 +19,15 @@ namespace npeimanovic_wpf
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : RibbonWindow
     {
+        static bool flag =false;
+        CurrentUser curruser= new CurrentUser();
+        EmailSetup emailSetup = new EmailSetup();
+        UserProfile usrProf = new UserProfile();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +41,28 @@ namespace npeimanovic_wpf
         private void CurrentUser_Loaded(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        public void RibbonControl_SelectionChanged()
+        {
+            // Do something
+            mainLeft.Children.Add(emailSetup);
+            
+        }
+        public void RemoveEmailSetup()
+        {
+            mainLeft.Children.Clear();
+            
+        }
+        public void RemoveOldUserControl()
+        {
+            mainLeft.Children.Clear();
+            mainright.Children.Clear();
+        }
+        public void addUser()
+        {
+            mainLeft.Children.Add(curruser);
+            mainright.Children.Add(usrProf);
         }
     }
 }
--- npeimanovic_wpf/RibbonControl.xaml.cs
+++ npeimanovic_wpf/RibbonControl.xaml.cs
@@ -21,6 +21,8 @@ namespace npeimanovic_wpf
     /// </summary>
     public partial class RibbonControl : Ribbon
     {
+       
+
         public RibbonControl()
         {
             InitializeComponent();
@@ -28,7 +30,29 @@ namespace npeimanovic_wpf
         private void add_clicked(object sender, RoutedEventArgs e)
         {
             UserControlAddEdit subWindow = new UserControlAddEdit();
+            
             subWindow.Show();
         }
+        private void edit_clicked(object sender, RoutedEventArgs e)
+        {
+            UserControlAddEdit subWindow = new UserControlAddEdit();
+            subWindow.title.Content = "Edit User";
+            subWindow.Show();
+        }  
+        
+        private void selchanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Email_Setup.IsSelected)
+            {
+                ((MainWindow)System.Windows.Application.Current.MainWindow).RemoveOldUserControl();
+                ((MainWindow)System.Windows.Application.Current.MainWindow).RibbonControl_SelectionChanged();
+            }
+            else
+            {
+                ((MainWindow)System.Windows.Application.Current.MainWindow).RemoveEmailSetup();
+               ((MainWindow)System.Windows.Application.Current.MainWindow).addUser();
+            }
+        }
+
     }
 }
